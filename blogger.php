c800a7feecffe7f161bb76ecd82e5a5ce5307ce5
thomasheaney <thomas@thomasheaney.com>
2024-06-07 12:11:28 +0100

    Add files via upload

--- blogger.php
+++ blogger.php
@@ -2,18 +2,8 @@
 
 //https://en.wikipedia.org/wiki/Old_Sarum_Castle
 
+include_once("./config.php");
 
-$blogs = array();
-$blogs["341832496341749426"] = "Somewhere The Tea's Getting Cold";
-$blogs["4122834195131500531"] = "The Black Castle (WarlordPaul)";
-$blogs["4266200265116124990"] = "Goblin Lee";
-$blogs["3283315887973123007"] = "Realm of Chaos 80s";
-$blogs["5135517998184661387"] = "Where The Sea Pours Out";
-$blogs["2812208972474813089"] = "Give Em Lead";
-$blogs["59617730633617003"] = "Tales From The Big Board";
-$blogs["1958522416503442248"] = "Coins and Scrolls";
-$blogs["2221418482531324115"] = "Lead Plague";
-$blogs["3298356674700006690"] = "The Leadpile";
 
 
 $blogId = isset($_GET["blogid"]) && !empty($_GET["blogid"]) ? $_GET["blogid"] : null;
@@ -28,6 +18,10 @@ $content = "";
 if(!isset($_GET["blogid"])){
   foreach($blogs as $k => $v){
     echo '<li><a href="./blogger.php?blogid=' . $k .'">' . $v. '</a></li>';
+  }
+
+  foreach($manual as $k => $v){
+    echo '<li><a href="./' . $k .'">' . $v. '</a></li>';
   }
   die();
 }
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$blogs = array();
+$blogs["341832496341749426"] = "Somewhere The Tea's Getting Cold";
+$blogs["4122834195131500531"] = "The Black Castle (WarlordPaul)";
+$blogs["4266200265116124990"] = "Goblin Lee";
+$blogs["3283315887973123007"] = "Realm of Chaos 80s";
+$blogs["5135517998184661387"] = "Where The Sea Pours Out";
+$blogs["2812208972474813089"] = "Give Em Lead";
+$blogs["59617730633617003"] = "Tales From The Big Board";
+$blogs["1958522416503442248"] = "Coins and Scrolls";
+$blogs["2221418482531324115"] = "Lead Plague";
+$blogs["3298356674700006690"] = "The Leadpile";
+$blogs["1083215263228559326"] = "Mars Miniatures";
+
+$manual = array();
+$manual["mword"] = "The M Word";
+
+?>
