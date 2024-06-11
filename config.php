389e0c0109872a23f0997988b0d08d476171276b
thomasheaney <thomas@thomasheaney.com>
2024-06-11 17:45:59 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -5,14 +5,18 @@ $blogs["341832496341749426"] = "Somewhere The Tea's Getting Cold";
 $blogs["4122834195131500531"] = "The Black Castle (WarlordPaul)";
 $blogs["4266200265116124990"] = "Goblin Lee";
 $blogs["3283315887973123007"] = "Realm of Chaos 80s";
+$blogs["8905698989087969872"] = "Snickit's Tail";
+$blogs["3071854826133661871"] = "Erny's place";
+$blogs["2034522422050340887"] = "Warhammer For Adults";
 $blogs["5135517998184661387"] = "Where The Sea Pours Out";
+$blogs["7366826811750659881"] = "Blue's Marauding Miniatures";
 $blogs["2812208972474813089"] = "Give Em Lead";
 $blogs["59617730633617003"] = "Tales From The Big Board";
 $blogs["1958522416503442248"] = "Coins and Scrolls";
 $blogs["2221418482531324115"] = "Lead Plague";
 $blogs["3298356674700006690"] = "The Leadpile";
 $blogs["1083215263228559326"] = "Mars Miniatures";
-
+//$blogs[""] = "";
 $manual = array();
 $manual["mword"] = "The M Word";
 
